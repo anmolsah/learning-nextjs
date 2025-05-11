2a582a08516f7218875ee0c7348a6e12eeb58c7a
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-11 14:03:25 +0530

    feat: replace static headings with dynamic links in Navbar component

--- learning-routing/src/components/navbar.tsx
+++ learning-routing/src/components/navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const NavbarPage = () => {
   return (
     <div>
       <nav className="container flex items-center justify-between mx-auto h-14 p-8">
-        <h1 className="font-bold text-xl">Home</h1>
-        <div className="flex gap-2">
-          <h1>Performance</h1>
-          <h1>Reliability</h1>
+        <Link href="/" className="font-bold text-xl">Home</Link>
+        <div className="flex gap-2 cursor-pointer">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
         </div>
       </nav>
     </div>
