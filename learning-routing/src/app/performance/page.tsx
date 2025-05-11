28d016595791bf37d8b423ba76853207afc72e64
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-11 13:59:28 +0530

    fix: correct capitalization in Performance and Reliability page headings

--- learning-routing/src/app/performance/page.tsx
+++ learning-routing/src/app/performance/page.tsx
@@ -3,7 +3,7 @@
 const PerformancePage = ()=>{
     return (
         <div>
-            <h1 className="text-3xl text-center mt-4 mb-4">performance page</h1>
+            <h1 className="text-3xl text-center mt-4 mb-4">Performance page</h1>
         </div>
     )
 }
--- learning-routing/src/app/reliability/page.tsx
+++ learning-routing/src/app/reliability/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ReliabilityPage() {
   return (
     <div>
-        <h1 className="text-3xl text-center mt-4 mb-4">something about reliability</h1>
+        <h1 className="text-3xl text-center mt-4 mb-4">Something about reliability</h1>
     </div>
   )
 }
